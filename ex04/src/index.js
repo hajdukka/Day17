e720a316e77d403c20016a7d8bba2f24e0e48352
hajdukka <nerminhajdo@gmail.com>
2021-03-24 21:09:26 +0100

    dodao ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,20 @@
+const BAKING_TEMPERATURES = {
+  cake: { low: 180, high: 250 },
+  bread: { low: 150, high: 220 },
+  pie: { low: 200, high: 230 },
+};
+
+function main(baking) {
+  //
+
+  const {
+    bread: { low: lowBread, high: hihgBread },
+  } = BAKING_TEMPERATURES;
+
+  return { lowBread, hihgBread };
+
+  //
+}
+
+console.log(main(BAKING_TEMPERATURES));
+module.exports = main;
